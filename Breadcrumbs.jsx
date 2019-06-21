4e0da9604974c89aa8f7c9b613bdc7682c4c9520
Micheal Pearce <michealpearce@outlook.com>
2019-06-21 15:26:07 -0400

    Some clean up

--- Breadcrumbs.jsx
+++ Breadcrumbs.jsx
@@ -6,48 +6,68 @@ import classNames from 'classnames'
 import styles from './styles.module.scss'
 import transitions from './transitions.module.scss'
 
-console.log(transitions)
+let TransCrumb = (props) => {
+
+	let crumb = props.crumb
+	let delay = props.delay
+
+	return (
+		<CSSTransition
+			key={crumb}
+			timeout={delay}
+			classNames={{
+				enter: transitions.enter,
+				enterActive: transitions.enterActive,
+				enterDone: transitions.enterDone,
+				exit: transitions.exit,
+				exitActive: transitions.exitActive
+			}}
+		>
+			<NavCrumb crumb={crumb} delay={delay} link={link} />
+		</CSSTransition>
+	)
+}
+
+let NavCrumb = (props) => {
+
+	let crumb = props.crumb
+	let delay = (props.delay) ? props.delay : 0
+	let link  = props.link
+
+	return (
+		<div className={styles.crumb}
+		style={{'transitionDuration': delay+'ms'}}>
+			<NavLink exact to={link} 
+			activeClassName={styles.activeLink}>
+				{crumb}
+			</NavLink>
+		</div>
+	)
+
+}
 
 export default class Breadcrumbs extends Component {
 	
 	generateBreadCrumbs(path) {
 		
-		let list = []
+		let crumbEls = []
 		let crumbs = path.split('/').filter(Boolean)
 		let link = '/'
 		let transDelay = 0
 
 		crumbs.forEach(crumb => {
 			link += crumb+'/'
-			list.push(
-				<CSSTransition
-					key={crumb}
-					timeout={transDelay}
-					classNames={{
-						enter: transitions.enter,
-						enterActive: transitions.enterActive,
-						enterDone: transitions.enterDone,
-						exit: transitions.exit,
-						exitActive: transitions.exitActive
-					}}
-				>
-					<div className={styles.crumb}
-						style={{
-							'transitionDuration': transDelay+'ms'
-						}}
-					>
-						<NavLink exact to={link} 
-							activeClassName={styles.activeLink}
-						>
-							{crumb}
-						</NavLink>
-					</div>
-				</CSSTransition>
+			crumbEls.push(
+				<NavCrumb
+					crumb={crumb}
+					delay={transDelay}
+					link={link}
+				/>
 			)
 			transDelay += 25
 		})
 
-		return list
+		return crumbEls
 		
 	}
 
@@ -55,20 +75,9 @@ export default class Breadcrumbs extends Component {
 
 		let props = this.props
 
-		let homeCrumbClasses = classNames([
-			styles.crumb,
-			(props.path === '/') ? styles.homeCrumb : ''
-		])
-
 		return (
 			<div className={styles.breadcrumbs}>
-				<div className={homeCrumbClasses}>
-					<NavLink exact to="/"
-						activeClassName={styles.activeLink}
-					>
-						Home
-					</NavLink>
-				</div>
+				<NavCrumb crumb="Home" link="/" />
 				<TransitionGroup className={styles.container} >
 					{this.generateBreadCrumbs(props.path)}
 				</TransitionGroup>
